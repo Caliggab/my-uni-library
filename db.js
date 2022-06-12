d032fce357ebf46c5bf463829d361db00a03a195
gabriel <gabriel@trezlabs.com>
2022-06-12 14:00:47 -0600

    hallo

--- db.js
+++ db.js
@@ -11,19 +11,13 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; //heroku addons
+prodConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addon
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
-const pool =
-  process.env.NODE_ENV === "production"
-    ? new Pool({
-        connectionString: proConfig,
-
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      })
-    : new Pool({
-        connectionString: devConfig,
-      });
+const pool = new Pool(proConfig);
 
 module.exports = pool;
