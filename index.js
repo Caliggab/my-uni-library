54e8006add91d9847ae2a20db66fbda031a16793
gabriel <gabriel@trezlabs.com>
2022-06-12 13:18:04 -0600

    hello!

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-console.log(__dirname);
-console.log(path.join(__dirname, "client/build"));
+
 
 //-------Routes-----//
 
