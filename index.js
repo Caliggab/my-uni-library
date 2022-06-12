aa6992fe6bb9fd91cb16026fd1e3b20fcca7826d
gabriel <gabriel@trezlabs.com>
2022-06-12 14:42:05 -0600

    so

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 const pool = require("./db");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 //
 
 //middleware
 app.use(cors());
 app.use(express.json()); // => allows us to access the req.body
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(express.static(path.join(__dirname, "client/build")));
 // app.use(express.static("./client/build")); => for demonstration
